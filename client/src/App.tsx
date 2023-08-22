4e5e6f59e35ff68d6a5971fcd9c54b5e9283accb
Hat52 <tanolihamzaali@gmail.com>
2023-08-22 10:45:52 +0500

    todo input field added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="h-screen w-screen flex flex-col items-center">
+			<h1 className="text-xl font-bold">Todo</h1>
+			<div className="flex items-end gap-5">
+				<div className="flex flex-col gap-2">
+					<label htmlFor="todo-input">Enter New Todo</label>
+					<input
+						id="todo-input"
+						type="text"
+						className="outline-none px-2 focus:shadow border h-[40px] rounded-[8px] "
+						placeholder="Enter todo"
+					/>
+				</div>
+				<button className="h-[40px] bg-blue-500 rounded-[8px] px-5 uppercase font-medium text-white">
+					Add
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
